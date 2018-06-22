be2eb79755ed6a8aaae3eac5006bc5115c340db9
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-06-22 12:03:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import ToggleButton from './components/ToggleButton'
+import Address from './components/Address'
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  //<ToggleButton />,
+  <Address
+    street="Caffrey Ave"
+    city="Far Rockaway" />,
   document.getElementById('root')
 )
